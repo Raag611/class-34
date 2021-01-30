27207c0ef2290cd9f166796f188a7e7856ef112b
Raag611 <73942393+Raag611@users.noreply.github.com>
2021-01-30 16:57:53 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,38 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.7,
+            length: 200,
+            damping : 0.05
+        }
+      
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+   
+    
+    
+    
+
+    display(){
+      
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            
+            stroke(48,22,8);
+           
+                line(pointA.x , pointA.y, pointB.x , pointB.y);
+                
+           
+           
+            
+            pop();
+        }
+    }
+    
+}
